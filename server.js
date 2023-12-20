7e61ec0eb06317385f57128ff0672b19add271e7
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-20 14:32:21 +0100

    added getAllJobs

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 const app = express();
+
+let jobs = [
+  {
+    id: nanoid(),
+    position: 'Software Engineer',
+    company: 'Amazon',
+    location: 'San Francisco, CA',
+  },
+  {
+    id: nanoid(),
+    position: 'Frontend Developer',
+    company: 'Google',
+    location: 'Jakarta, Indonesia',
+  },
+];
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(5100, () => {
-    console.log('hello world');
-})
+app.get('/jobs', (req, res) => {
+  res.status(200).json({ jobs });
+});
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log('server is running on port ' + port);
+});
