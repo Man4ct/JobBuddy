9931aa09db33de300a88d6b23143a46063ba963b
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-12 20:07:19 +0100

    add a formRowSelect

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,4 +3,5 @@ export { default as FormRow } from './FormRow'
 export { default as BigSidebar } from './BigSidebar'
 export { default as Navbar } from './Navbar'
 export { default as SmallSidebar } from './SmallSidebar'
+export { default as FormRowSelect } from './FormRowSelect'
 
